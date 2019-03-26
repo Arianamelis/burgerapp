96202f098f922064ba11e38581088d024f7c4f38
Ariana Melis <arianamelis@gmail.com>
2019-03-25 17:15:14 -0700

    server working

---
+++ burger/db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO burgers (burger_name) VALUES ("Texas burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Pizza burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Chocolate burger", false);
+
+
+
+
+
